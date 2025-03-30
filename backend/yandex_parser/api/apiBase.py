45d559fef80368a0a4179119271e5339cbac9d34
mrnykterstein <mrnykterstein@yandex-staff.ru>
2025-03-31 02:23:56 +0300

    - feat logs: add api queries logging - feat requirements: add requirements.txt

--- backend/yandex_parser/api/apiBase.py
+++ backend/yandex_parser/api/apiBase.py
@@ -1,3 +1,4 @@
+from logs.logs import log_middleware
 from yandex_parser.api.login import link, token
 from yandex_parser.api.problems import problems
 from yandex_parser.api.standings import standings
@@ -7,20 +8,26 @@ from yandex_parser.parse_submissions.submissionParser import parse_submissions
 
 
 class ApiYandex:
-
-    get_link = link
-    get_token = token
+    register_link = link
+    register_token = token
 
     @staticmethod
-    def get_standings(oauth: str, contest_id: str, from_pos: int = None, to_pos: int = None):
-        names = problems(oauth, contest_id)
-        results = standings(oauth, contest_id, from_pos, to_pos)
+    @log_middleware
+    async def login(login: str, password: str):
+        pass
+
+    @staticmethod
+    @log_middleware
+    async def get_standings(oauth: str, contest_id: str, from_pos: int = None, to_pos: int = None):
+        names = await problems(oauth, contest_id)
+        results = await standings(oauth, contest_id, from_pos, to_pos)
 
         return parse_results(results, names)
 
     @staticmethod
-    def get_submissions(oauth: str, contest_id: str, from_pos: int = None, to_pos: int = None):
-        names = problems(oauth, contest_id)
-        submissions_list = submissions(oauth, contest_id, from_pos, to_pos)
+    @log_middleware
+    async def get_submissions(oauth: str, contest_id: str, from_pos: int = None, to_pos: int = None):
+        names = await problems(oauth, contest_id)
+        submissions_list = await submissions(oauth, contest_id, from_pos, to_pos)
 
         return parse_submissions(submissions_list, names)
--- backend/yandex_parser/api/standings.py
+++ backend/yandex_parser/api/standings.py
@@ -1,11 +1,10 @@
-import requests
+from yarl import URL
+from aiohttp import ClientSession
 
-from yandex_parser.appCredentials import yandex_host
-
-DEFAULT_PAGE_SIZE = 100
+from settings import YANDEX_HOST, DEFAULT_PAGE_SIZE
 
 
-def standings(token: str, contest_id: str, from_pos: int = None, to_pos: int = None):
+async def standings(token: str, contest_id: str, from_pos: int = None, to_pos: int = None):
     headers = {
         'Authorization': f'OAuth {token}'
     }
@@ -20,26 +19,26 @@ def standings(token: str, contest_id: str, from_pos: int = None, to_pos: int = N
         to_pos = from_pos + DEFAULT_PAGE_SIZE
         params['pageSize'] = to_pos
 
-    result = requests.get(yandex_host + '/contests/' + contest_id + '/standings', params=params, headers=headers)
+    url = URL(YANDEX_HOST) / 'contests' / contest_id / 'standings'
 
-    if result.status_code == 200:
-        result = result.json()
+    async with ClientSession() as client:
+        async with client.get(str(url), params=params, headers=headers) as response:
+            if response.status == 200:
+                result = await response.json()
 
-        standings_slice = result['rows']
+                standings_slice = result['rows']
 
-        if from_pos is not None:
-            from_pos -= 1
-            standings_slice = standings_slice[from_pos:to_pos]
+                if from_pos is not None:
+                    from_pos -= 1
+                    standings_slice = standings_slice[from_pos:to_pos]
 
-        result['rows'] = standings_slice
-        return result
-    elif result.status_code == 400:
-        raise PermissionError('Standings are not generated!')
-    elif result.status_code == 403:
-        raise PermissionError('You do not have permission to this contest!')
-    elif result.status_code == 404:
-        raise PermissionError('Contest is not found!')
-    else:
-        # save_log(OAuthToken.json()['error_description'], 'while getting standings',
-        # token, contest_id, from_pos, to_pos)
-        raise RuntimeError('Oops! Something went wrong. We are already working to fix it!')
+                result['rows'] = standings_slice
+                return result
+            elif response.status == 400:
+                raise PermissionError('Standings are not generated!')
+            elif response.status == 403:
+                raise PermissionError('You do not have permission to this contest!')
+            elif response.status == 404:
+                raise PermissionError('Contest is not found!')
+            else:
+                raise RuntimeError('Oops! Something went wrong. We are already working to fix it!')
