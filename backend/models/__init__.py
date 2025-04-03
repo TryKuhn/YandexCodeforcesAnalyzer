af4e09e61bea42ba86c2b5a3a369a3569afd5917
TryKuhn <egor.workspace@gmail.com>
2025-04-03 23:02:59 +0300

    fix CI errors

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,3 @@
-from user import User
-from contest_result import ContestResult
-from participant_attempt import ParticipantsAttempt
-from base import Base
+from models.user import User
+from models.contest_result import ContestResult
+from models.participant_attempt import ParticipantsAttempt
