47d2f6cf99e47427b3c1dfc2cf1108b53dca62e5
egor.anisevich <Egor.Anisevich@x5.ru>
2025-12-12 22:56:49 +0300

    add more functionality (models, api, tokens)

--- backend/api/user_api.py
+++ backend/api/user_api.py
@@ -1,34 +1,41 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
-from backend.api.schemas import UserRegister, UserLogin, Token
 
+from backend.api.schemas import UserRegister, UserLogin, Token
 from backend.app.database import get_db
 
 router = APIRouter()
+
 
 @router.get("/health")
 async def health() -> dict:
     return {"status": "ok"}
 
+
 @router.post("/register")
 async def register(payload: UserRegister, db: Session = Depends(get_db)) -> dict:
     return {"message": "Welcome back!"}
+
 
 @router.post("/login")
 async def login(user: UserLogin, db: Session = Depends(get_db)) -> dict:
     return {"message": "Login successful!"}
 
+
 @router.post("/logout")
 async def logout(user: UserLogin, db: Session = Depends(get_db)) -> dict:
     return {"message": "Logout successful!"}
 
+
 @router.post("/refresh")
-async def register(user: Token, db: Session = Depends(get_db)) -> dict:
+async def refresh(user: Token, db: Session = Depends(get_db)) -> dict:
     return {"message": "Welcome back!"}
+
 
 @router.post("/link_codeforces")
 async def link_codeforces(user: Token, db: Session = Depends(get_db)) -> dict:
     return {"message": "Codeforces account linked!"}
+
 
 @router.post("/link_yandex")
 async def link_yandex(user: Token, db: Session = Depends(get_db)) -> dict:
--- backend/app/server.py
+++ backend/app/server.py
@@ -3,28 +3,25 @@ from contextlib import asynccontextmanager
 from logging import basicConfig, INFO
 
 from fastapi import FastAPI
-
 from sqlalchemy.exc import OperationalError
 
-from backend.models.base import Base
 from backend.api.user_api import router
+from backend.app.database import engine
+from backend.models.base import Base
 from settings import ANALYZER_ROOT
 
-from backend.app.database import engine
-
-
-
 basicConfig(
-    filename=ANALYZER_ROOT/"logs.txt",
+    filename=ANALYZER_ROOT / "logs.txt",
     level=INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     for _ in range(5):
         try:
-            with engine.connect() as conn:
+            with engine.connect():
                 pass
             break
         except OperationalError:
@@ -35,6 +32,7 @@ async def lifespan(app: FastAPI):
     yield
 
     engine.dispose()
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
